979fc30158b334a41f9f4e5b232586c419dd7e8a
Maxime Delorme <m.delorme@surrey.ac.uk>
2017-05-29 13:51:44 +0100

    Test

---
+++ exercises/01_hello_world/hello_world.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <mpi.h>
+
+int main(int argc, char** argv) {
+  // Initialisation
+
+  // Reading size and rank
+
+  // Printing
+  std::cout << "Hello world, from process #" << ... << std::endl;
+  
+  // Finalisation
+
+
+  return 0;
+}
---
+++ exercises/01_hello_world/hello_world.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+echo -e "\033[1mCompiling\033[0m"
+
+rm -rf out hw
+mpicxx -o hw hello_world.cpp 2> err_log
+mkdir out
+rc=$?
+if [[ $rc != 0 ]]; then
+    echo -e "\033[1mError\033\0m : mpicxx returned"
+    cat err_log
+    echo "TECHIO> success false"
+    rm -rf out
+    exit $rc
+fi
+echo "Compilation is OK"
+
+rm -rf out/*
+rc=0
+
+echo -e "\033[1mExecution\033[0m"
+mpirun -np 4 -output-filename out/out ./hw
+
+for f in `ls out`; do
+    echo " **** Output of $f : ****"
+    cat out/$f
+
+echo "TECHIO> success true"
+
+rm -rf out
+
