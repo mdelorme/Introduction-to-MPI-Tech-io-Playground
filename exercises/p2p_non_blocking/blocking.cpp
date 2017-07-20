5b15f0fd7fe52355395fd692c51d073214b229e1
Maxime Delorme <m.delorme@surrey.ac.uk>
2017-07-20 12:10:48 +0100

    Fixed the blobking vs non_blocking example

--- exercises/p2p_non_blocking/blocking.cpp
+++ exercises/p2p_non_blocking/blocking.cpp
@@ -1,7 +1,7 @@
 // This file is provided as an example of blocking scenario. Please don't modify it !
 void play_blocking_scenario() {
   // Initialising buffer :
-  for (int i=0; i < 10; ++i)
+  for (int i=0; i < buffer_count; ++i)
     buffer[i] = (rank == 0 ? i*i : 0);
 
   MPI_Barrier(MPI_COMM_WORLD);
@@ -10,11 +10,8 @@ void play_blocking_scenario() {
   // Simulate working
   if (rank == 0) {
     sleep(3);
-    std::cout << "0 " << time + MPI_Wtime() << std::endl;
     MPI_Send(buffer, buffer_count, MPI_INT, 1, 0, MPI_COMM_WORLD);
-    std::cout << "Send finished" << std::endl;
     sleep(6);
-    std::cout << "0 " << time + MPI_Wtime() << std::endl;
     
     // Modifying the buffer for second step
     for (int i=0; i < buffer_count; ++i)
@@ -24,12 +21,9 @@ void play_blocking_scenario() {
   }
   else {
     sleep(5);
-    std::cout << "receiving" << std::endl;
     MPI_Recv(buffer, buffer_count, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    print_buffer(time, 1, buffer, buffer_count); // This function prints the buffer for process 1
     sleep(3);
     MPI_Recv(buffer, buffer_count, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    print_buffer(time, 2, buffer, buffer_count);
   }
 
   time += MPI_Wtime();
--- exercises/p2p_non_blocking/non_blocking.cpp
+++ exercises/p2p_non_blocking/non_blocking.cpp
@@ -4,7 +4,7 @@ void play_non_blocking_scenario() {
   int request_finished = 0;
 
   // Initialising buffer :
-  for (int i=0; i < 10; ++i)
+  for (int i=0; i < buffer_count; ++i)
     buffer[i] = (rank == 0 ? i*i : 0);
 
   // Starting the chronometer
@@ -17,67 +17,61 @@ void play_non_blocking_scenario() {
     sleep(3);
 
     // 1- Initialise the non-blocking send to process 1
-    MPI_Isend(buffer, buffer_count, MPI_INT, 1, 0, MPI_COMM_WORLD, &request);
+    // [...]
+
     double time_left = 6000.0;
     while (time_left > 0.0) {
       usleep(1000); // We work for 1ms
 
       // 2- Test if the request is finished (only if not already finished)
-      if (!request_finished)
-	MPI_Test(&request, &request_finished, &status);
-
+      // [...]
+      
       // 1ms left to work
       time_left -= 1000.0;
     }
 
     // 3- If the request is not yet complete, wait here. 
-    if (!request_finished)
-      MPI_Wait(&request, &status);
+    // [...]
 
     // Modifying the buffer for second step
     for (int i=0; i < buffer_count; ++i)
       buffer[i] = -i;
 
     // 4- Prepare another request for process 1 with a different tag
-    MPI_Isend(buffer, buffer_count, MPI_INT, 1, 1, MPI_COMM_WORLD, &request);
+    // [...]
     
     time_left = 3000.0;
     while (time_left > 0.0) {
       usleep(1000); // We work for 1ms
 
       // 5- Test if the request is finished (only if not already finished)
-      if (!request_finished)
-	MPI_Test(&request, &request_finished, &status);
+      // [...]
 
       // 1ms left to work
       time_left -= 1000.0;
     }
     // 6- Wait for it to finish
-    if (!request_finished)
-      MPI_Wait(&request, &status);
+    // [...]
+    
   }
   else {
     // Work for 5 seconds
     sleep(5);
 
     // 7- Initialise the non-blocking receive from process 0
-    MPI_Irecv(buffer, buffer_count, MPI_INT, 0, 0, MPI_COMM_WORLD, &request);
+    // [...]
 
     // 8- Wait here for the request to be completed
-    MPI_Wait(&request, &status);
-
-    print_buffer(time, 1, buffer, buffer_count); // This function prints the buffer for process 1
-
+    // [...]
+    
     // Work for 3 seconds
     sleep(3);
 
     // 9- Initialise another non-blocking receive
-    MPI_Irecv(buffer, buffer_count, MPI_INT, 0, 1, MPI_COMM_WORLD, &request);
-
+    // [...]
+    
     // 10- Wait for it to be completed
-    MPI_Wait(&request, &status);
-
-    print_buffer(time, 2, buffer, buffer_count);
+    // [...]
   }
   ////////// should not modify anything AFTER this point //////////
 
