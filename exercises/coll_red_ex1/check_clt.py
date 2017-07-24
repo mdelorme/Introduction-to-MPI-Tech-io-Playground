b10d1cc95025f7534e9e57f33e9eab9d7504768e
Maxime Delorme <m.delorme@surrey.ac.uk>
2017-07-24 08:46:19 +0100

    Changed working dir for viewer

--- exercises/coll_red_ex1/check_clt.py
+++ exercises/coll_red_ex1/check_clt.py
@@ -18,7 +18,7 @@ plt.subplot(212)
 plt.hist(norm, bins=50)
 plt.title('Summed distribution')
 plt.savefig('histograms.png')
-print('TECHIO> open --static-dir /project/target index.html')
+print('TECHIO> open --static-dir /project/target/coll_red_ex1 index.html')
 
 print('Computing Shapiro test')
 print(' H0 : The distribution follows a Normal distribution')
