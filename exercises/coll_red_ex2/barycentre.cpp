089566b8b118ba0607a4a8edfd3bca799c2d27dc
Maxime Delorme <m.delorme@surrey.ac.uk>
2017-07-31 19:06:14 +0100

    Removed the contents of the exercise

--- exercises/coll_red_ex2/barycentre.cpp
+++ exercises/coll_red_ex2/barycentre.cpp
@@ -3,33 +3,21 @@ void compute(int total_count, int my_count, float my_points[][3]) {
   // my_count is the number of points for this process
   // my_points is a float table of shape [my_count][3]
 
-  // 1- Sum over all the points
+  // 1- Sum over all the points in local_sum
   float local_sum[3] = {0.0f, 0.0f, 0.0f};
-  for (int i=0; i < my_count; ++i) {
-    for (int j=0; j < 3; ++j) {
-      //  std::cout << local_sum[0] << " " << local_sum[1] << " " << local_sum[2] << std::endl;
-      local_sum[j] += my_points[i][j];
-    }
-  }
 
-  // 2- Reduction
+  // 2- Reduce the sum of all the points on the variable barycentre 
   float barycentre[3];
-  MPI_Allreduce(local_sum, barycentre, 3, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD);
   
-  // 3- Dividing by the number of points
-  for (int j=0; j < 3; ++j)
-    barycentre[j] /= total_count;
+  // 3- Divide every component of the barycentre by the number of points
 
-  // 4- Computing the distance
+  // For every point
   for (int i=0; i < my_count; ++i) {
     float dist = 0.0f;
-    
-    for (int j=0; j < 3; ++j) {
-      float diff = barycentre[j] - my_points[i][j];
-      dist += diff * diff;
-    }
-    dist = sqrt(dist);
 
+    // 4- Compute the distance for every point
+    
+    // And printing the result
     std::cout << rank << " " << dist << std::endl;
   }
 }
