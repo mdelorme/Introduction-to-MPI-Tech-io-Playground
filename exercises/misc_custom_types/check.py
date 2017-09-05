5ab71eb464060d3929fe44abc19802e54fd26e66
Maxime Delorme <m.delorme@surrey.ac.uk>
2017-09-05 15:39:07 +0100

    Stripped the strings to remove line feed

--- exercises/misc_custom_types/check.py
+++ exercises/misc_custom_types/check.py
@@ -3,8 +3,8 @@ f_2 = open('out', 'r')
 
 for i, lines in enumerate(zip(f_1, f_2)):
     l1, l2 = lines
-    print('TECHIO> message --channel "Your program" "{}"'.format(l2))
-    print('TECHIO> message --channel "Solution" "{}"'.format(l1))
+    print('TECHIO> message --channel "Your program" "{}"'.format(l2.strip()))
+    print('TECHIO> message --channel "Solution" "{}"'.format(l1.strip()))
 
     if l1 != l2: 
         print('Error, line {}'.format(i))
